675e9d6f4d4d78df9cd8b614b69f0d2b74b96bef
Luis Oliveira <luis.andre@psychometrika.com.br>
2025-03-21 09:52:56 -0300

    Refactor app.js and create server.js for improved structure and clarity

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-import { validateShortURL } from "./urlValidator.js";
 import express from "express";
+import { validateShortURL } from "./urlValidator.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.post("/validate-url", (req, res) => {
   const { url } = req.body;
   const isValid = validateShortURL(url);
   res.status(isValid ? 200 : 400).json({ valid: isValid });
-});
-
-const PORT = process.env.PORT || 3333;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 export default app;
